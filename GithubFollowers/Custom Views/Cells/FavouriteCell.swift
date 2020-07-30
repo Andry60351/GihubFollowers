1dbc5e1f64f4c89ad95d05ad3c311cc8c1798401
Andry60351 <49724349+Andry60351@users.noreply.github.com>
2020-07-30 05:20:39 -0700

    Update FavouriteCell.swift

--- GithubFollowers/Custom Views/Cells/FavouriteCell.swift
+++ GithubFollowers/Custom Views/Cells/FavouriteCell.swift
@@ -10,39 +10,30 @@ import UIKit
 
 class FavouriteCell: UITableViewCell {
 
-    
-    // MARK: - properties
     static let reuseID  = "FavouriteCell"
     let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel   = GFTitleLabel(textAlignment: .left, fontSize: 26)
     
-    // MARK: - override init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configure()
     }
     
-    // MARK: - required init
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    // MARK: - awakeFromNib
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     
-    // MARK: - setSelected
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
-    // MARK: - configure
     private func configure() {
         addSubview(avatarImageView)
         addSubview(usernameLabel)
@@ -63,7 +54,6 @@ class FavouriteCell: UITableViewCell {
         ])
     }
     
-    // MARK: - set
     func set(favourite: Follower) {
         usernameLabel.text = favourite.login
             NetworkManager.shared.downloadImage(from: favourite.avatarUrl) { [weak self] image in
