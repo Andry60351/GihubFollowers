ba45fdddce9ce4610021612b8d010f906773769e
Andry60351 <49724349+Andry60351@users.noreply.github.com>
2020-07-30 05:20:03 -0700

    Update GFButton.swift

--- GithubFollowers/Custom Views/Buttons/GFButton.swift
+++ GithubFollowers/Custom Views/Buttons/GFButton.swift
@@ -10,25 +10,21 @@ import UIKit
 
 class GFButton: UIButton {
     
-    // MARK: - override init
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
-    // MARK: - required init
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - init
     convenience init(backgroundColor: UIColor, title: String) {
         self.init(frame: .zero)
         self.backgroundColor = backgroundColor
         self.setTitle(title, for: .normal)
     }
     
-    // MARK: - configure
     private final func configure() {
         layer.cornerRadius = 10
         setTitleColor(.white, for: .normal)
@@ -36,7 +32,6 @@ class GFButton: UIButton {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
-    // MARK: - set
     func set(backgroundColor: UIColor, title: String) {
         self.backgroundColor = backgroundColor
         setTitle(title, for: .normal)
