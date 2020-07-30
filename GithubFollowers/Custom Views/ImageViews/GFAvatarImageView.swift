22f6ad930ae71644c73afefc883d8387e8ab44b1
Andry60351 <49724349+Andry60351@users.noreply.github.com>
2020-07-30 05:21:29 -0700

    Update GFAvatarImageView.swift

--- GithubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
+++ GithubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
@@ -13,18 +13,15 @@ class GFAvatarImageView: UIImageView {
     let cache = NetworkManager.shared.cache
     let placeHolderImage = UIImage(named: "avatar-placeholder")!
     
-    // MARK: - override init
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
-    // MARK: - required init
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - configure
     private func configure() {
         layer.cornerRadius = 10
         clipsToBounds = true
